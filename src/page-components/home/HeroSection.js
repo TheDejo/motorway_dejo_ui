8d3d0e82a15490a8c73818f1f789b9c97d99d556
DEJO <divineogbe@gmail.com>
2023-04-02 00:26:32 +0100

    add hero section

---
+++ src/page-components/home/HeroSection.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HeroSection = () => {
+    return (
+        <div className='motorway-hero'>This is a hero section</div>
+    )
+}
+
+export default HeroSection
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import FormSection from "../../page-components/home/FormSection";
+import HeroSection from "../../page-components/home/HeroSection";
 import HomeSection from "../../page-components/home/HomeSection";
 
 const Home = () => {
     return (
         <section className="home">
+            <HeroSection />
             <HomeSection />
             <FormSection />
         </section>
